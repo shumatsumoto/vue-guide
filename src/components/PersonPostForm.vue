d610f62e89297bc240c7ca21703d0f6430162bab
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-07-30 11:06:21 +0900

    persons component 作成

---
+++ src/components/PersonPostForm.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="form-container">
+    <div class="input-container">
+      <div>
+        <span>name:</span>
+        <input type="text" class="input" />
+      </div>
+      <div>
+        <span>age:</span>
+        <input type="text" class="input" />
+      </div>
+    </div>
+    <button class="register-button">register</button>
+  </div>
+</template>
+
+<style scoped>
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgb(255, 241, 226);
+  padding: 24px 0;
+  width: 50%;
+  margin-bottom: 12px;
+  border-radius: 4px;
+}
+</style>
